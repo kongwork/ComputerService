19b35ce9351fbc6c26b87ae61d5cf1544c9027c5
kongwork <kongwork26729@gmail.com>
2022-03-16 19:41:10 +0700

    first commit

---
+++ models/device.js
@@ -0,0 +1,31 @@
+// use mongoose
+const mongoose = require('mongoose')
+
+// connect MongoDB
+const dbUrl = "mongodb://localhost:27017/mydb"
+mongoose.connect(dbUrl, {
+    useNewUrlparser: true,
+    useUnifiedTopology: true
+}).catch(err => console.log(err))
+
+// design Schema
+let deviceSchema = mongoose.Schema({
+    DeviceName: String,
+    CategoryID: { type: mongoose.Schema.Types.ObjectId, ref: 'categorys' },
+    DeviceCode: String,
+    Room: String,
+    Price: Number,
+    Date: Date,
+    DeviceStatus: String
+});
+
+// create model
+let Device = mongoose.model("devices", deviceSchema)
+
+// export model
+module.exports = Device
+
+//for save data
+module.exports.saveDevice = function (model, data) {
+    model.save(data)
+}
---
+++ routes/edit_device.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const router = express.Router()
+const Device = require("../models/device")
+const Category = require("../models/category")
+
+
+router.post("/editDevice", (req, res) => {
+    const edit_device = req.body.edit_device
+    const showname = req.session.username
+    if (req.session.login) {
+        Device.findOne({ _id: edit_device }).exec((err, doc_d) => {
+            Category.find().exec((err, doc_c) => {
+                res.render("edit_device", { devices: doc_d, categorys: doc_c, showname: showname })
+            })
+        })
+    }
+    else {
+        res.redirect('/')
+    }
+})
+
+// Update Data Device
+router.post("/updateDevice", (req, res) => {
+    // ข้อมูลใหม่ที่ส่งมาจาก form edit
+    const update_device = req.body.device_id
+    let data = {
+        CategoryName: req.body.category
+    }
+    // อัพเดตข้อมูล Device
+    Device.findByIdAndUpdate(update_device, data, { useFindAndModify: false }).exec(err => {
+        console.log(req.body)
+        res.redirect("/device")
+    })
+})
+
+module.exports = router;
